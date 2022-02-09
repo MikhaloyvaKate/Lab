78745af5c1c585025e1c31c40416bf30f51c81f0
Mikhaloyva Kate <mikhaloyvakate@gmail.com>
2022-02-09 12:41:06 +0200

    Added button processing and text output to the desired blocks

---
+++ lab.js
@@ -0,0 +1,12 @@
+document.getElementById("button");
+
+function convert() {
+  let textBox_1 = document.getElementById("block_1").value;
+  formatingText(textBox_1);
+}
+
+function formatingText(text) {
+  for (i = 0; i < length.textBox_1; i++) {}
+}
+
+let textBox_2 = (document.getElementById("block_2").value = textBox_1);
